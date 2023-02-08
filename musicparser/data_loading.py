a2a93ab8834b9d5944d973f3448bbd625ef7af85
fosfrancesco <foscarin.francesco@gmail.com>
2023-02-08 19:01:43 +0100

    first data loader

--- musicparser/data_loading.py
+++ musicparser/data_loading.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import pandas as pd
 import xml.etree.ElementTree as ET
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import partitura as pt
 
 class TSDataset(Dataset):
     """Dataset for the TS trees.
@@ -11,14 +13,17 @@ class TSDataset(Dataset):
         Args:
             data_folder (string): Path to the folder containing the data.
         """
-        self.data_df = get_data_df(data_folder)
-        self.data_df["ts_tree"] = self.data_df["ts"].apply(ts_xml_to_dependency_tree)
-
+        data_df = get_data_df(data_folder)
+        self.score_files = data_df["score"]
+        self.ts_files = data_df["ts"]
+        self.note_features = [get_score_features(score) for score in self.score_files]
+        self.tss = [ts_xml_to_dependency_tree(ts) for ts in self.ts_files]
+        
     def __len__(self):
-        return len(self.data_df)
+        return len(self.score_files)
 
     def __getitem__(self, idx):
-        return self.data_df.iloc[idx]
+        return self.note_features[idx], self.tss[idx] 
 
 def get_score_path(piece_folder):
     """Retrieve the path to the score file from a piece folder.
@@ -81,3 +86,43 @@ def get_score_features(score_file):
     Returns:
         list: a list of notes, each note is a tuple with features
     """
+    score = pt.load_musicxml(score_file)
+    # get the note array
+    na = pt.utils.music.ensure_notearray(
+        score,
+        include_metrical_position=True,
+        include_time_signature=True,    
+    )
+    # correct the metrical information
+    clean_na, real_ts, real_measure_duration = correct_metrical_information(na)
+    # get the note features
+    note_feature = get_features_from_na(clean_na)
+    return note_feature
+
+    
+def correct_metrical_information(na):
+    """Corrects the metrical information in the note array. This removes wrong ts and metrical info for pickup notes and ending measures."""
+    time_signatures = np.char.array(na["ts_beats"].astype(str)) + np.char.array(["/"]*na.shape[0])+ np.char.array(na["ts_beat_type"].astype(str))
+    # get real time signature and measure duration, discarding pickup and ending measure ts
+    real_ts = np.unique(time_signatures)[-1]
+    real_measure_duration = na[time_signatures == real_ts][0]["tot_measure_div"]
+    # find pickup notes
+    pickup_note_indices = np.where((na["tot_measure_div"]!= real_measure_duration) * (na["onset_div"] < real_measure_duration ))[0]
+    # set the pickup note to the correct metrical position
+    na["rel_onset_div"][pickup_note_indices] = na["onset_div"][pickup_note_indices] + real_measure_duration - na["tot_measure_div"][pickup_note_indices]
+    # set the measure duration to correct value
+    na["tot_measure_div"] = real_measure_duration
+    # set the correct time signature
+    na["ts_beats"] = real_ts.split("/")[0]
+    na["ts_mus_beats"] = real_ts.split("/")[0]
+    na["ts_beat_type"] = real_ts.split("/")[1]
+    return na, real_ts, real_measure_duration
+
+def get_features_from_na(na):
+    """Extracts the features from the note array. It must contains the metrical information."""
+    pitch = na["pitch"]
+    metrical =  na["is_downbeat"] #TODO: add more metrical info
+    duration = na["duration_div"]/na["tot_measure_div"]
+    # TODO: consider rests as lag from previous note
+    return np.vstack((pitch, metrical, duration)).T
+
